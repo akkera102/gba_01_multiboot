db3634b33ec9882285f4c80e810d1014d31c1a76
akkera102 <akkera102@gmail.com>
2016-05-20 03:59:52 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,52 @@
+Raspberry Pi GBA Loader - Boot up a GameBoy Advance using Multiboot (no cart req.)
+
+## Description
+The GBA file that you wish to load into the GBA is stored on the raspberry pi local file system.
+
+1. run multiboot program
+2. turn on GBA
+3. next is nothing. very simple! :D
+
+
+## Wiring
+GBA connector
+
+  T
+1 3 5         1 3v, xxx 3 SI, wht 5 SC, red
+2 4 6         2 SO, blk 4 SD, grn 6 GN, xxx
+
+GBA   --- raspberry pi b+
+
+6-GND     GND
+3-SI      GPIO 10 (SPI_MOSI)
+2-SO      GPIO  9 (SPI_MISO)
+5-SC      GPIO 11 (SPI_SCLK)
+
+
+## command log
+$ gcc multiboot.c -lwiringPi -o multiboot
+$ ./multiboot
+
+Looking for GBA 0x72026202
+0x72026202 0x00006202  ; Found GBA
+0x72026202 0x00006102  ; Recognition OK
+Send Header(NoDebug)
+0x00020000 0x00006200  ; Transfer of header data complete
+0x72026200 0x00006202  ; Exchange master/slave info again
+0x72026202 0x000063d1  ; Send palette data
+0x73c563d1 0x000063d1  ; Send palette data, receive 0x73hh****
+0x73c563d1 0x000064d4  ; Send handshake data
+0x739564d4 0x000013c0  ; Send length info, receive seed 0x**cc****
+Send encrypted data(NoDebug)
+Wait for GBA to respond with CRC 0x00750065
+0x00750065 0x00000066  ; GBA ready with CRC
+0x5a470066 0x00005a47  ; Let's exchange CRC!
+CRC ...hope they match!
+MulitBoot done
+
+
+## Author
+
+Ken Kaarvik    kkaarvik@yahoo.com    Nov 13, 2010
+akkera102                            Nov 08, 2014(mbed version)
+akkera102                            May 20, 2016(raspberry pi version)
---
+++ src/multiboot.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <wiringPi.h>
+
+uint32_t WriteSPI32NoDebug(uint32_t w)
+{
+	uint8_t buf[4];
+
+	buf[3] = (w & 0x000000ff);
+	buf[2] = (w & 0x0000ff00) >>  8;
+	buf[1] = (w & 0x00ff0000) >> 16;
+	buf[0] = (w & 0xff000000) >> 24;
+
+	wiringPiSPIDataRW(0, &buf, 4);
+
+	uint32_t r = 0;
+
+	r += buf[0] << 24;
+	r += buf[1] << 16;
+	r += buf[2] <<  8;
+	r += buf[3];
+
+	return r;
+}
+
+uint32_t WriteSPI32(uint32_t w, char* msg)
+{
+	uint32_t r = WriteSPI32NoDebug(w);
+
+	printf("0x%08x 0x%08x  ; %s\n", r, w, msg);
+	return  r;
+}
+
+void WaitSPI32(uint32_t w, uint32_t comp, char* msg)
+{
+	printf("%s 0x%08x\n", msg, comp);
+	uint32_t r;
+
+	do
+	{
+		r = WriteSPI32NoDebug(w);
+		usleep(10000);
+
+	} while(r != comp);
+}
+
+void main(void)
+{
+	FILE *fp = fopen("test.gba", "rb");
+	if(fp == NULL)
+	{
+		printf("Err: Can't open file\n");
+		return;
+	}
+
+	fseek(fp, 0L, SEEK_END);
+	long fsize = (ftell(fp) + 0x0f) & 0xfffffff0;
+
+	if(fsize > 0x40000)
+	{
+		printf("Err: Max file size 256kB\n");
+		return;
+	}
+
+	fseek(fp, 0L, SEEK_SET);
+	long fcnt = 0;
+
+	uint32_t r, w, w2;
+	uint32_t i, bit;
+
+	wiringPiSPISetupMode(0, 100000, 3);
+
+	WaitSPI32(0x00006202, 0x72026202, "Looking for GBA");
+
+	r = WriteSPI32(0x00006202, "Found GBA");
+	r = WriteSPI32(0x00006102, "Recognition OK");
+
+	printf("Send Header(NoDebug)\n");
+	for(i=0; i<=0x5f; i++)
+	{
+		w = getc(fp);
+		w = getc(fp) << 8 | w;
+		fcnt += 2;
+
+		r = WriteSPI32NoDebug(w);
+	}
+
+	r = WriteSPI32(0x00006200, "Transfer of header data complete");
+	r = WriteSPI32(0x00006202, "Exchange master/slave info again");
+
+	r = WriteSPI32(0x000063d1, "Send palette data");
+	r = WriteSPI32(0x000063d1, "Send palette data, receive 0x73hh****");  
+
+	uint32_t m = ((r & 0x00ff0000) >>  8) + 0xffff00d1;
+	uint32_t h = ((r & 0x00ff0000) >> 16) + 0xf;
+
+	r = WriteSPI32((((r >> 16) + 0xf) & 0xff) | 0x00006400, "Send handshake data");
+	r = WriteSPI32((fsize - 0x190) / 4, "Send length info, receive seed 0x**cc****");
+
+	uint32_t f = (((r & 0x00ff0000) >> 8) + h) | 0xffff0000;
+	uint32_t c = 0x0000c387;
+
+
+	printf("Send encrypted data(NoDebug)\n");
+
+	while(fcnt < fsize)
+	{
+		w = getc(fp);
+		w = getc(fp) <<  8 | w;
+		w = getc(fp) << 16 | w;
+		w = getc(fp) << 24 | w;
+
+		w2 = w;
+
+		for(bit=0; bit<32; bit++)
+		{
+			if((c ^ w) & 0x01)
+			{
+				c = (c >> 1) ^ 0x0000c37b;
+			}
+			else
+			{
+				c = c >> 1;
+			}
+
+			w = w >> 1;
+		}
+
+		m = (0x6f646573 * m) + 1;
+		WriteSPI32NoDebug(w2 ^ ((~(0x02000000 + fcnt)) + 1) ^m ^0x43202f2f);
+
+		fcnt = fcnt + 4;
+	}
+	fclose(fp);
+
+	for(bit=0; bit<32; bit++)
+	{
+		if((c ^ f) & 0x01)
+		{
+			c =( c >> 1) ^ 0x0000c37b;
+		}
+		else
+		{
+			c = c >> 1;
+		}
+
+		f = f >> 1;
+	}
+
+	WaitSPI32(0x00000065, 0x00750065, "Wait for GBA to respond with CRC");
+
+	r = WriteSPI32(0x00000066, "GBA ready with CRC");
+	r = WriteSPI32(c,          "Let's exchange CRC!");
+
+	printf("CRC ...hope they match!\n");
+	printf("MulitBoot done\n");
+}
